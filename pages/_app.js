80316b4cba2ad73392849baffeaeb43338e8fce1
xbliu <xbliu@kaikeba.com>
2019-10-16 15:26:26 +0800

    update _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import withRedux from 'next-redux-wrapper';
 import * as Sentry from '@sentry/browser';
 import initStore from '../store';
