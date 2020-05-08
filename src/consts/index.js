88d33daeec05648dcc1bd12f98cc34fe51ab4438
starkwang <wsd312@163.com>
2020-05-08 15:52:10 +0800

    feat: init

--- src/consts/index.js
+++ src/consts/index.js
@@ -7,7 +7,7 @@
  * @Description: file content
  */
 // eslint-disable-next-line import/prefer-default-export
-export const SENTRY_DSN = 'https://d000a97c650e45fbbfa8b828d7a2ad8f@am.1.com/2';
+export const SENTRY_DSN = 'https://xxxxxxx@am.1.com/2';
 
 export const BOOTSTRAP =
   'https://img.kaikeba.com/style/96b9082f-bootstrap-grid.min.css';
